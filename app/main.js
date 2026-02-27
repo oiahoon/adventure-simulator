34c554c15113b9a2b8f0841ab04ad1784b073292
Joey Huang <johuang@aligntech.com>
2026-02-27 22:53:58 +0800

    fix(cache): bust stale homepage bundle after removing challenge UI

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-import { createGameUI } from "../ui/game-ui.js";
+import { createGameUI } from "../ui/game-ui.js?v=20260227_1";
 
 const STORAGE_KEY = "wechat-survival-best";
 const TARGET_DAY = 100;
