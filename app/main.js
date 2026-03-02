6e69e049f6745756327febd762ae7298699f2a5d
Joey Huang <johuang@aligntech.com>
2026-03-02 11:06:01 +0800

    bump web cache version to 20260228_29

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-import { createGameUI } from "../ui/game-ui.js?v=20260228_28";
+import { createGameUI } from "../ui/game-ui.js?v=20260228_29";
 
 const STORAGE_KEY = "wechat-survival-best";
 const TARGET_DAY = 100;
